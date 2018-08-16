e22d7aed65d3af5c4493c84d9fc87d8b756f6658
Matthew Hitchman <m.hitchman@outlook.com>
2018-08-16 22:02:19 +0100

    Accept template project name as cli arg

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include "palv.h"
+#include <clara.hpp>
 #include <iostream>
 #include <string>
+#include <tuple>
+
+struct cliArguments
+{
+    std::string templateName = "app";
+    std::string projectName;
+};
+
+cliArguments setCLI(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-    std::string projectName = palverlib::parseCommandLineArg(argc, argv);
+    auto [templateName, projectName] = setCLI(argc, argv);
+
     try
     {
 	auto homeDir = palverlib::findHomeDir();
 	auto configDir = palverlib::findConfigDir(homeDir);
-	auto templateDir = palverlib::findTemplateDir(configDir);
+	auto templateDir = palverlib::findTemplateDir(configDir, templateName);
 	palverlib::copyTemplateProjectToCWD(templateDir, projectName);
     }
     catch(palverlib::file_not_found_error& e)
@@ -20,4 +31,17 @@ int main(int argc, char* argv[])
     {
 	std::cerr << e.what() << std::endl;
     }
+}
+
+cliArguments setCLI(int argc, char* argv[])
+{
+    cliArguments args;
+    auto cli
+	= clara::Opt(args.templateName, "Template name")
+	["-n"]["--new"]
+	("Type of project to create")
+	| clara::Arg(args.projectName, "Project name")
+	("Name of the created project");
+    cli.parse(clara::Args(argc, argv));
+    return args;
 }
